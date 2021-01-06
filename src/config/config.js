5f77093ddf6a397e42dfbad63063e0d83f6dcaa5
azusa5526 <azusa5526@gmail.com>
2021-01-06 15:58:44 +0800

    connection success

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,12 @@ require('dotenv').config();
 
 const envVarSchema = Joi.object({
   NODE_ENV: Joi.string().default('development'), // 字串且預設值為development 並只允許兩種參數
-  PORT: Joi.number().default(8080) // 數字且預設值為 8080
+  PORT: Joi.number().default(8080), // 數字且預設值為 8080
+  MYSQL_PORT: Joi.number().default(3306), // 數字且預設值為3306
+  MYSQL_HOST: Joi.string().default('127.0.0.1'), // 字串取預設值為127.0.0.1
+  MYSQL_USER: Joi.string(), // 字串
+  MYSQL_PASS: Joi.string(), // 字串
+  MYSQL_DATABASE: Joi.string()
 }).required();
 
 // process.env 撈取 .env 內的變數做 joi 驗證
@@ -19,18 +24,28 @@ const envVarSchema = Joi.object({
 
 const envVars = envVarSchema.validate({
   NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  MYSQL_PORT: process.env.MYSQL_PORT, // 數字且預設值為3306
+  MYSQL_HOST: process.env.MYSQL_HOST, // 字串取預設值為127.0.0.1
+  MYSQL_USER: process.env.MYSQL_USER, // 字串
+  MYSQL_PASS: process.env.MYSQL_PASS, // 字串
+  MYSQL_DATABASE: process.env.MYSQL_DATABASE
 });
 
 if (envVars.error) {
   throw envVars.error;
 }
 
-console.log('envVars', envVars);
-
 const config = {
   env: envVars.value.NODE_ENV,
-  port: envVars.value.PORT
+  port: envVars.value.PORT,
+  mysqlPort: envVars.value.MYSQL_PORT, // 連接阜號(MYSQL_PORT)
+  mysqlHost: envVars.value.MYSQL_HOST, // 主機名稱 (MYSQL_HOST)
+  mysqlUserName: envVars.value.MYSQL_USER, // 用戶名稱 (MYSQL_USER)
+  mysqlPass: envVars.value.MYSQL_PASS, // 資料庫密碼(MYSQL_PASS)
+  mysqlDatabase: envVars.value.MYSQL_DATABASE // 資料庫名稱(MYSQL_DATABASE)
 };
+
+console.log(config);
 
 export default config; // 匯出共用
--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -1,13 +1,32 @@
-import express from 'express';
+/* eslint-disable no-unreachable */
 
+import express from 'express';
+import mysql from 'mysql';
 import config from './../../config/config';
 
 const router = express.Router();
 
-
 /* GET localhost:[port]/api page. */
 router.get('/', (req, res) => {
   res.send(`此路徑是: localhost:${config.port}/api`);
+});
+
+router.get('/sqlTest', (req, res) => {
+  const pool = mysql.createPool({
+    host: config.mysqlHost,
+    user: config.mysqlUserName,
+    password: config.mysqlPass,
+    database: config.mysqlDatabase
+  });
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.send(err);
+      console.log('連線失敗！');
+    } else {
+      res.send('連線成功！');
+      console.log(connection);
+    }
+  });
 });
 
 export default router;
